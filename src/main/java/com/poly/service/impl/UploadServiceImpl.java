eee0b8261fbee13ab06d1cdd39d458480d54cc27
LeBaoDang <85282838+LeBaoDANG@users.noreply.github.com>
2022-08-08 20:30:26 +0700

    .

--- src/main/java/com/poly/service/impl/UploadServiceImpl.java
+++ src/main/java/com/poly/service/impl/UploadServiceImpl.java
@@ -1,10 +1,15 @@
 package com.poly.service.impl;
 
 import java.io.File;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.management.RuntimeErrorException;
 import javax.servlet.ServletContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -18,21 +23,22 @@ public class UploadServiceImpl implements UploadService {
 	
 	@Override
 	public File save(MultipartFile file, String folder) {
-		File dir = new File(app.getRealPath("/assets/" + folder));
-		if (!dir.exists()) {
-			dir.mkdirs();
-		}
-		// tạo ra folder nếu chưa tồn tại
-		String s = System.currentTimeMillis() + file.getOriginalFilename();
-		String name = Integer.toHexString(s.hashCode()) + s.substring(s.lastIndexOf("."));
-		// tiến hành luu file vào trong folder
 		try {
+			File dir = new ClassPathResource("static/assets/" + folder).getFile();
+			//File dir = Paths.get(app.getRealPath("/assets/"), folder).toFile();
+			if (!dir.exists()) {
+				dir.mkdirs();
+			}
+			// tạo ra folder nếu chưa tồn tại
+			String s = System.currentTimeMillis() + file.getOriginalFilename();
+			String name = Integer.toHexString(s.hashCode()) + s.substring(s.lastIndexOf("."));
+			// tiến hành luu file vào trong folder
 			File saveFile = new File(dir,name);
 			file.transferTo(saveFile);
 			System.out.println(saveFile.getAbsolutePath());
 			return saveFile;
 		} catch (Exception e) {
-			throw new RuntimeException(e);
+			throw new RuntimeException();
 		}
 	}
 
